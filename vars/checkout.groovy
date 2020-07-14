14e58895e17d3e1e2d815b0b3db83b39cf5dd5d7
DevopsRizwan <rizwan418360@gmail.com>
2020-07-14 18:22:54 +0400

    Update checkout.groovy

--- vars/checkout.groovy
+++ vars/checkout.groovy
@@ -1,15 +1,13 @@
 def call() {
 
-        /*checkout([$class: 'GitSCM',
+        checkout([$class: 'GitSCM',
           userRemoteConfigs: [[name: 'origin', url: env.GIT_URL, credentialsId: ' ']],
           branches: [[name: env.GIT_BRANCH]],
           extensions: [
             [$class: 'CloneOption', shallow: false, timeout: 60],
             [$class: 'WipeWorkspace']
           ]])
-      */
-        
-        checkout([$class: 'GitSCM', branches: [[name: 'master']],
-    userRemoteConfigs: [[url: 'https://github.com/DevopsRizwan/spring-helm-demo.git']]])
+     
+
         
         }
