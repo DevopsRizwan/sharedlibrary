0216324794ac136c567ceb04864ff9f59a0b3dd7
Rizwan <rizvanjavid@gmail.com>
2020-07-15 13:53:25 +0400

    test

---
+++ vars/gitCheckout.groovy
@@ -0,0 +1,7 @@
+checkout([$class: 'GitSCM',
+          userRemoteConfigs: [[name: 'origin', url: env.GIT_URL, credentialsId: ' ']],
+          branches: [[name: env.GIT_BRANCH]],
+          extensions: [
+            [$class: 'CloneOption', shallow: false, timeout: 60],
+            [$class: 'WipeWorkspace']
+          ]])
