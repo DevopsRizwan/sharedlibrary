50db0e1a41f7e1b213e57abe8d886c2b364612da
DevopsRizwan <rizwan418360@gmail.com>
2020-07-15 17:36:05 +0400

    Update st_pipeline_ci.groovy

--- vars/st_pipeline_ci.groovy
+++ vars/st_pipeline_ci.groovy
@@ -1,41 +1,31 @@
 import org.devopsgate.*
-
-
 def call(Map param){
 
-
 pipeline{
-
 agent any
-
-
-
 stages {
 
 stage("clone") {
-
-steps{
+  steps{
      
-checkout([$class: 'GitSCM',
+    checkout([$class: 'GitSCM',
           userRemoteConfigs: [[name: 'origin', url: param.git_url, credentialsId: ' ']],
           branches: [[name: param.git_branch]],
           extensions: [
             [$class: 'CloneOption', shallow: false, timeout: 60],
             [$class: 'WipeWorkspace']
-          ]])
+          ]])  
+      }
 }
-
-}
-
-}
+     
 
 stage("Build") {
 
-steps{
+   steps{
       
-def buildfun = new Build(this)
+     def buildfun = new Build(this)
 
-buildfun.buildMvn("${param.mvnPath}")
+       buildfun.buildMvn("${param.mvnPath}")
 }
 
 }
