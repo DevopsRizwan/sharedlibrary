254b796d9d56a16e86edb7ba6674c60b8d68129b
Rizwan <rizvanjavid@gmail.com>
2020-07-15 16:43:29 +0400

    build funcetion added

--- src/org/devopsgate/Build.groovy
+++ src/org/devopsgate/Build.groovy
@@ -14,7 +14,7 @@ import static groovy.json.JsonOutput.*
 * @param mvnPath -path of the projects pom.xml file 
 */
   def buildMvn( mvnPath ) {
-    steps.sh "mvn  clean install -Pprod -f ${mvnPath} -Dmaven.test.skip=true -Dmaven.test.failure.ignore=true"
+    steps.sh "echo ${mvnPath}"
   }
 
 /**
--- vars/my_function.groovy
+++ vars/my_function.groovy
@@ -1,6 +1,16 @@
-def build_ci (String buildType)
+import org.devopsgate.*
+/*def build_ci (String buildType)
 
 {
  println "$buildType"
+
+}
+*/
+
+def call(Map param){
+
+def buildfun = new Build(this)
+
+buildfun.buildMvn("${param.mvnPath}")
 
 }
