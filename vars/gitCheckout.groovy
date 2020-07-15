c70e31022ab0386ab83755a7a11c1ae33f718410
DevopsRizwan <rizwan418360@gmail.com>
2020-07-15 14:03:21 +0400

    Update gitCheckout.groovy

--- vars/gitCheckout.groovy
+++ vars/gitCheckout.groovy
@@ -3,12 +3,15 @@ import static groovy.json.JsonOutput.*
 
 // vars/sayHello.groovy
 def call(Map params) {
+          
+          print "test"
 
-checkout([$class: 'GitSCM',
+/*checkout([$class: 'GitSCM',
           userRemoteConfigs: [[name: 'origin', url: env.GIT_URL, credentialsId: ' ']],
           branches: [[name: env.GIT_BRANCH]],
           extensions: [
             [$class: 'CloneOption', shallow: false, timeout: 60],
             [$class: 'WipeWorkspace']
           ]])
+          */
 }
