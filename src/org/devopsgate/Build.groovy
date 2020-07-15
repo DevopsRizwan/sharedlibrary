137dc4806ada3965329efc0e21d9349b21a94049
DevopsRizwan <rizwan418360@gmail.com>
2020-07-15 16:59:30 +0400

    Update Build.groovy

--- src/org/devopsgate/Build.groovy
+++ src/org/devopsgate/Build.groovy
@@ -14,7 +14,7 @@ import static groovy.json.JsonOutput.*
 * @param mvnPath -path of the projects pom.xml file 
 */
   def buildMvn( mvnPath ) {
-    steps.sh "mvn clean install -f  ${mvnPath}" -Dmaven.test.skip=true
+    steps.sh "mvn clean install -f  ${mvnPath} -Dmaven.test.skip=true"
   }
 
 /**
