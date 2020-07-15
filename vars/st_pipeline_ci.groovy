5855ba83d916d2bab43ebd9f871e42a109de0593
Rizwan <rizvanjavid@gmail.com>
2020-07-15 17:25:18 +0400

    test

---
+++ vars/st_pipeline_ci.groovy
@@ -0,0 +1,45 @@
+import org.devopsgate.*
+
+
+def call(Map param){
+
+
+pipeline{
+
+agent any
+
+
+
+stages {
+
+stage(clone) {
+
+steps{
+     
+checkout([$class: 'GitSCM',
+          userRemoteConfigs: [[name: 'origin', url: param.git_url, credentialsId: ' ']],
+          branches: [[name: param.git_branch]],
+          extensions: [
+            [$class: 'CloneOption', shallow: false, timeout: 60],
+            [$class: 'WipeWorkspace']
+          ]])
+}
+
+}
+
+}
+
+stage(clone) {
+
+steps{
+      
+def buildfun = new Build(this)
+
+buildfun.buildMvn("${param.mvnPath}")
+}
+
+}
+
+}
+
+}
